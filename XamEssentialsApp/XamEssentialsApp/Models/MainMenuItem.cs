1c3056facc091adce2eae57eecb174ea661a0a13
Ndumiso_suspect <73614263+Bhekinkosi12@users.noreply.github.com>
2021-03-29 14:39:38 -0700

    Update MainMenuItem.cs

--- XamEssentialsApp/XamEssentialsApp/Models/MainMenuItem.cs
+++ XamEssentialsApp/XamEssentialsApp/Models/MainMenuItem.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using SQLite;
 
 namespace XamEssentialsApp.Models
 {
     public class MainMenuItem
     {
+        [PrimaryKey]
+         
+        public int ID { get; set; }
         public string Title { get; set; }
 
         public string IconSource { get; set; }
